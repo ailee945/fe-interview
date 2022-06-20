5d9c016cd204c711d9d3083e11cc010f86e39d55
Ailee <ailee945@outlook.com>
2022-06-20 23:22:26 +0800

    feat(vue):v-model

--- vue/learn-scss/src/components/IndexItem.vue
+++ vue/learn-scss/src/components/IndexItem.vue
@@ -1,25 +1,13 @@
 <template>
-  <div class="p1"></div>
-  <div class="p2"></div>
-  <div class="p3"></div>
-  <div class="p4"></div>
-  <div class="p5"></div>
+  <VModel v-model="query" />
+  <h2>query:{{query}}</h2>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import VModel from "./VModel.vue";
 
-<style lang="scss" scoped>
-div {
-  display: inline-block;
-  width: 20px;
-  height: 20px;
-  margin: 5px;
-}
-$colors: red green yellow skyblue black;
-@each $color in $colors {
-  $index: index($colors, $color);
-  .p#{$index} {
-    background-color: $color;
-  }
-}
-</style>
+const query = ref('')
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue/learn-scss/src/components/VModel.vue
@@ -0,0 +1,24 @@
+<template>
+  <input type="text" v-model="query" />
+  <h2>子组件:{{query}}</h2>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const emit = defineEmits(['update:modelValue'])
+const props = defineProps({modelValue:{
+  type:String
+}})
+
+const query = computed({
+  get(){
+    return props.modelValue
+  },
+  set(newValue){
+    emit('update:modelValue',newValue)
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
